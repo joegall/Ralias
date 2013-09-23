047da698e0ef99f84f0551e2f81f62188d464bdb
Quentin Geissmann <qgessmann@gmail.com>
2013-09-23 12:10:52 +0100

    A first draft with the few aliases I could think of

---
+++ draft.R
@@ -0,0 +1,11 @@
+#~  PURE ALIASES
+a <- head
+z <- tail
+ipkg <- install.packages
+p <- print
+
+
+#~ UTILITIES
+last <- function(x){x[length(x)]}
+rmall <- function() rm(envir=.GlobalEnv,list=ls(envir=.GlobalEnv)) #Remove all objects from the global environment
+pwd() <- function(print(getwd())) #looks like unix function
