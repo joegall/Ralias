dc48a720e46bf4645bbd1adf7b060fc70b56dffa
Quentin Geissmann <qgessmann@gmail.com>
2013-09-23 12:14:36 +0100

    ibid

--- draft.R
+++ draft.R
@@ -8,4 +8,4 @@ p <- print
 #~ UTILITIES
 last <- function(x){x[length(x)]}
 rmall <- function() rm(envir=.GlobalEnv,list=ls(envir=.GlobalEnv)) #Remove all objects from the global environment
-pwd <- function(print(getwd())) #looks like unix function
+pwd <- function() print(getwd()) #looks like unix function
